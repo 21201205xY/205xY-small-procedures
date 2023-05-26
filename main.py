3315684f6a668d191efb10fa25ffde5a224deea5
21201205xY <111406812+21201205xY@users.noreply.github.com>
2023-05-26 11:02:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ import pyautogui
 
 
 def copy():
-    print('此程序会自动读取你电脑剪切板的内容并模拟键盘输出\n(暂不支持中文模拟输入)')
     with open('settings.txt', 'r', encoding='utf-8') as f:
         f_read = f.readlines()
         for line in f_read:
             ls = line.split(':')
             try:
                 _time = eval(ls[1])
-            except SyntaxError:
+            except:
                 _time = 0
     for i in range(5):
         print(f'还有{5 - i}秒进行模拟，请将光标移至要复制处')
-        time.sleep(0.1)
+        time.sleep(1)
     paste = pyperclip.paste()
     pyautogui.typewrite(f'{paste}', interval=_time)
 
 
 if __name__ == '__main__':
-    copy()
+    print(
+        '此程序开源，开源地址：https://github.com/21201205xY/205xY-small-procedures/tree/main\n'
+        '此程序会自动读取你电脑剪切板的内容并模拟键盘输出\n'
+        '(暂不支持中文模拟输入)\n'
+        '将要复制内容复制后，输入\'1\'以开始粘贴:'
+        , end='')
+    flag = eval(input())
+    while flag == 1:
+        copy()
+        flag = 0
